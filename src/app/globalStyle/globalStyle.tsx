33058e2d291e2db70280c46748e118856677328d
Tu Nombre <tuemail@example.com>
2024-05-24 20:48:51 -0300

    f gi

--- src/app/globalStyle/globalStyle.tsx
+++ src/app/globalStyle/globalStyle.tsx
@@ -1,4 +1,5 @@
-import styled, { createGlobalStyle } from "styled-components";
+
+import styled, { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   body {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,14 @@
 // Home.tsx
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 
-import { useAppDispatch, useAppSelector } from "@/redux/hooks"; // Asegúrate de ajustar la ruta de importación
-import {
-  fetchProducts,
-  selectVisibleProducts,
-} from "@/redux/features/product/productSlice"; // Asegúrate de que la ruta de importación sea correcta
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import Navbar from "./components/navbar/navbar";
 import Header from "./components/header/header";
-import { GlobalStyle, MainContainer } from "./globalStyle/globalStyle";
 function Home() {
-  const dispatch = useAppDispatch();
-  const products = useAppSelector(selectVisibleProducts);
-
-  useEffect(() => {
-    dispatch(fetchProducts());
-  }, [dispatch]);
-
   return (
     <>
-    <GlobalStyle></GlobalStyle>
-     <Navbar></Navbar>
-     <Header></Header>
-     <MainContainer></MainContainer>
+      <Navbar />
+      <Header />
     </>
   );
 }
