52829ba53aa6ef01943b52de603e9a2c64e5e475
Tu Nombre <tuemail@example.com>
2024-06-04 16:53:01 -0300

    g

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
 import Navbar from "../components/navbar/navbar";
-import Servicess from "../components/services&Calendar/services";
-import MyCalendar from "../components/services&Calendar/calendar/calendar";
+import Servicess from "./components/services&Calendar/services";
+import MyCalendar from "./components/services&Calendar/calendar/calendar";
 
 const Services: React.FC = () => {
   return (
